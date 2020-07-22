f7fce562f83c2b754b2c4d6d61798c6b1e4ce093
Karina Ture <karina@campus.technion.ac.il>
2020-07-22 21:36:24 +0300

    1st gui basically finished. extended search options (toggle). nav bar reacts correctly to screen resize and scroll.

--- js/gui.js
+++ js/gui.js
@@ -8,7 +8,7 @@ function scrollFunction() {
     let logo = document.getElementById("logo");
     let title = document.getElementsByClassName("navbar-title")[0];
 
-    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         navbar.style.padding = "5px 10px 5px 10px";
 
         // logo.style.padding = "5px 10px 5px 10px";
@@ -21,15 +21,24 @@ function scrollFunction() {
         // document.getElementById("subtitle").style.fontSize = "1em"; todo should be smaller?
 
     } else {
-        navbar.style.padding = "30px 10px 30px 10px";
+        navbar.style.padding = "30px 10px 10px 10px";
 
         // logo.style.padding = "30px 10px 30px 10px";
         // logo.style.margin = "30px 10px 30px 10px";
         logo.style.height = "10em";
         // logo.style.width = "10em";
 
-        title.style.padding = "30px 10px 30px 10px";
+        title.style.padding = "30px 10px 10px 10px";
         document.getElementById("title").style.fontSize = "2em";
         // document.getElementById("subtitle").style.fontSize = "1em"; todo should be smaller?
     }
 }
+
+
+function ShowHideDiv() {
+        var chkYes = document.getElementById("chkYes");
+        var search_subject = document.getElementById("search-subject");
+        var search_ref = document.getElementById("search-ref");
+        search_subject.style.display = chkYes.checked ? "block" : "none";
+        search_ref.style.display = chkYes.checked ? "none" : "block";
+    }
