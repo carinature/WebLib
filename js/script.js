e1b49c6e414961169b362a311d5d4d48a9e65b51
Karina Ture <karina@campus.technion.ac.il>
2020-07-26 23:14:53 +0300

    1st page done. some adjustments to nav bar. 2nd pg has collapsible filter section, but not with all the categories yet.

--- js/script.js
+++ js/script.js
@@ -22,7 +22,6 @@ function search_button_function() {
     // elem = anchor.insertAdjacentElement("afterend", elem);
     // elem.textContent = 'pipi';
 
-      location.replace("../html/search-results.html")
+      location.replace("/home/rbd/workspace/web/web_lib_project/html/search-results.html")
 
 }
-
