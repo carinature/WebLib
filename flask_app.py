559bd13a4956f06e7d3bfcf8e0e0496cf8d83eac
karinature <karina@campus.technion.ac.il>
2020-08-13 19:12:14 +0300

    structure arrangement to fit flask

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,13 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return '<h1>You did IT!</h1><h1>You did IT!</h1>'
 
+@app.route('/')
+@app.route('/index.html')
+def hello_world():
+    # return '<h1>You did IT!</h1><h1>You did IT!</h1><h1>You did IT!</h1>'
+    return render_template('index.html')
