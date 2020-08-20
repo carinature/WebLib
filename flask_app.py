63f26290af5cacd9b55784d18e0b52e8c6ba65d9
karinature <karina@campus.technion.ac.il>
2020-08-20 18:56:15 +0300

    index supports multi form

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template, make_response, redirect, url_for, request
 from flask_wtf import FlaskForm
 
@@ -47,9 +47,6 @@ class ExampleEntry(db.Model):
 #     login_pass = PasswordField('Password', [validators.Required()])
 
 
-
-
-
 # ++++++++++++  dbg page ++++++++++++
 @app.route('/check', methods=['GET', 'POST'])
 def check_check():
@@ -67,7 +64,7 @@ def check_check():
     return redirect('/check')
 
 
-@app.route('/search-results')
+@app.route('/search-results', methods=['GET', 'POST'])
 def search_results():
     return render_template('search-results.html')
 
@@ -84,10 +81,58 @@ def subject_list():
     # return render_template(url_for('subject_list')+'.html')
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+# @app.route('/#', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+# @app.route('/index', methods=['GET', 'POST'])
+# @app.route('/index#', methods=['GET', 'POST'])
+def home():
+    if 'GET' == request.method:
+        print('INDEX - GET')
+        return render_template('index.html')
+
+    print('INDEX - POST')
+    print(request.form.to_dict())
+    print(request.form)
+    print(request.form.keys().__str__())
+    print()
+
+    fields = [
+        "search-subject",
+        "second-keyword",
+        "search-author",
+        "search-work",
+        "search-reference"
+    ]
+    # print("fields: ", fields)
+    print(request.form.get("search-subject"))
+    if fields[0] in request.form:
+        print('INDEX - fields[0]')
+        print(request.form.get(fields[0]))
+    else:
+        print("whhhhhhtttttttt")
+    if fields[1] in request.form:
+        print('INDEX - fields[1]')
+        print(request.form.get(fields[1]))
+    if fields[2] in request.form:
+        print('INDEX - fields[2]')
+        print(request.form.get(fields[2]))
+    #     print(request.form.to_dict())
+    # print(url_for('search_results'))
+    # print(url_for('home'))
+    # collect fields
+    # depending on which fields/form filled - choose a search function
+    # next page should receive the results of the function as params
+    # like so:         return render_template('check.html', comments=ExampleEntry.query.all())
+    # but first try like this         redirect(url_for("search_results", comments=ExampleEntry.query.all()))
+
+    # create a class for the forms?
+    # you MUST make sure to be safe from SQL injection and other X
+
+    # in the search-results do
+    #   change get to render with params
+    #   change html to create/fill new data ("you searched for...", "total #results..")
+    #   consider running the functions in "search-result" and NOT "home"
+    return redirect(url_for("search_results"))
 
 
 @app.errorhandler(404)
