03d65906721fc6bf5e6437c851501d08cb1b9744
karinature <karina@campus.technion.ac.il>
2020-08-20 16:03:30 +0300

    aesthetic & misc

--- static/js/gui.js
+++ static/js/gui.js
@@ -6,25 +6,27 @@ window.onscroll = function () {
 function scrollFunction() {
     let navbar = document.getElementsByClassName("navbar")[0];
     let logo = document.getElementById("logo");
+    let button_line = document.getElementById("navbarResponsive");
     let title = document.getElementsByClassName("navbar-title")[0];
 
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        // navbar.style.padding = "5px 10px 5px 10px";
         navbar.style.padding = "5px 10px 5px 10px";
-        navbar.style.padding = "5px 10px 5px 10px";
+        // navbar.style.padding = "0";
 
         // logo.style.padding = "5px 10px 5px 10px";
         // logo.style.margin = "5px 10px 5px 10px";
-        logo.style.height = "5rem";
+        logo.style.height = "7.5rem";
+        // logo.style.margin = "0";
         // logo.style.width = "5em";
-        // logo.style.margin = "10px";
-        title.style.transform = "scale(0.75)";
+        title.style.transform = "scale(0.5)";
+        title.style.margin = "0";
+        title.style.paddingTop = "0";
         // title.style.padding = "5px 10px 5px 10px";
         // document.getElementById("navbar-title").style.fontSize = "1em";
         // document.getElementById("subtitle").style.fontSize = "1em"; todo should be smaller?
 
     } else {
-        navbar.style.padding = "30px 10px 10px 10px";
+        // navbar.style.padding = "30px 10px 10px 10px";
 
         // logo.style.padding = "30px 10px 30px 10px";
         // logo.style.margin = "30px 10px 30px 10px";
@@ -32,7 +34,7 @@ function scrollFunction() {
         // logo.style.width = "10em";
 
         title.style.transform = "scale(1)";
-        title.style.padding = "30px 10px 10px 10px";
+        // title.style.padding = "30px 10px 10px 10px";
         // document.getElementById("navbar-title").style.fontSize = "2em";
         // document.getElementById("subtitle").style.fontSize = "1em"; todo should be smaller?
     }
@@ -40,9 +42,11 @@ function scrollFunction() {
 
 
 function ShowHideDiv() {
-    var chkYes = document.getElementById("chkYes");
-    var search_subject = document.getElementById("search-subject");
-    var search_ref = document.getElementById("search-ref");
+    let chkYes = document.getElementById("chkYes");
+    let search_subject = document.getElementById("search-subject");
+    let search_ref = document.getElementById("search-ref");
     search_subject.style.display = chkYes.checked ? "block" : "none";
+
     search_ref.style.display = chkYes.checked ? "none" : "block";
 }
+
