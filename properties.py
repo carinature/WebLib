ebb54890f7e6e14be4cd782e81d27314bac7e2fd
karinature <karina@campus.technion.ac.il>
2020-09-11 19:00:33 +0300

    added properties file

---
+++ properties.py
@@ -0,0 +1,23 @@
+import os
+
+# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{db_name}".format(
+#     username="karinature",
+#     password="dsmiUw2sn",
+#     hostname="karinature.mysql.pythonanywhere-services.com",
+#     db_name="karinature$tryout",
+# )
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{db_name}".format(
+    username="root",
+    password="123",
+    hostname="localhost",
+    db_name="tryout",
+)
+
+# global ROOT_DIR
+# global RAW_DATA_DIR
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__)) #FIXME - find absolute path
+RAW_DATA_DIR = os.path.join(ROOT_DIR, 'raw_data')
+TEMPLATES_DIR = os.path.join(ROOT_DIR, 'templates')
+STATIC_DIR = os.path.join(ROOT_DIR, 'templates')
+UTILS_DIR = os.path.join(ROOT_DIR, 'utilities')
+
