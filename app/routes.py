f53f569288eacdbe96346c14978edafe281b426e
karinature <karina@campus.technion.ac.il>
2020-10-05 14:57:06 +0300

    subject list page finished. close #6

--- app/routes.py
+++ app/routes.py
@@ -20,48 +20,54 @@ from flask import flash
 print('~' * 113)
 
 
-@app.route("/kaka", methods=['GET', 'POST'])
-def kaka():
-    # headers = {"Content-Type": "app/kaka"}
-    # return make_response(
-    #     'Test worked!',
-    #     200,
-    #     headers
-    # )
+# ++++++++++++  search results and filtering page ++++++++++++
+@app.route('/search-results/<string:search_word>', methods=['GET', 'POST'])
+@app.route('/search-results', methods=['GET', 'POST'])
+def search_results(search_word=''):
+    flash('You doing great GRRRLLLL')
 
-    subject_form = SearchSubject(request.form)
-    # reference_form = SearchReference(request.form)
-    # if subject_form.submit_subject.data and subject_form.validate_on_submit():
-    #     print('Great Success')
-    #     return redirect(url_for('success', title='subject_form'))
-    # if reference_form.submit_reference.data and reference_form.validate_on_submit():
-    #     print('Great Success')
-    #     return redirect(url_for('success', title='reference_form'))
-    #     # return render_template('kaka.html', title='Great Success', subject_form=subject_form)
-    # print('what happend')
-    # print("subject_form.errors")
-    # print(subject_form.errors)
-    # flash(subject_form.errors)
-    # page = request.args.get('page', 1, type=int)
-    # table = TextSubject.query.paginate(page, app.config['POSTS_PER_PAGE'], False).items
-    # return render_template('kaka.html',mydata=table, form1=subject_form)  # , form2=reference_form)
-    return render_template('kaka.html', form1=subject_form)  # , form2=reference_form)
+    # search_word = search_word if search_word else request.args['search_word']
+    print(search_word)
+    if 'GET' == request.method:
+        # print(request.args)
+        # print(request.values)
+        # print(request)
+        # flash('You \'GET\' this')
+        # print('get')
+        # print(request.args['results'])
+        # results = request.args['results']
+        # return render_template('search-results.html', results=results)
+
+        subject_form = SearchSubject(request.form)
+        reference_form = SearchReference(request.form)
+        filter_form = FilterForm(request.form)
+
+        return render_template('search-results.html', title=f'Search Results for: {search_word}',
+                               description="Tiresias: The Ancient Mediterranean Religions Source Database",
+                               results=['res1', 'res2', 'res3', 'res4'], total=0,
+                               form1=subject_form, form2=reference_form, form3=filter_form,
+                               )  # , results=results)
+
+    flash('You\'re \'POST\' on!')
+    print('post')
+    # print(request.args['results'])
+    # results = request.args['results']
+    return render_template('search-results.html', title=f'Search Result for: {search_word}',
+                           description="Tiresias: The Ancient Mediterranean Religions Source Database",
+                           results=['pupu'], total=0)
+
+    # todo
+    #  in the search-results do
+    #    consider running the functions in "search-result" and NOT "home"
+    #    make some sort "waiting" bar/circle/notification (search "flashing/messages" in the flask doc)
+    #  change projection to include entire entry instead of index alone
+    #  add fuzzy (returns things *like* but not necessarily the same) / regex search on the query
+    #  clean data before:
+    #    split creation of tables function in db-migration into multiple function
+    #    appropriate normalization
 
 
-@app.route("/pipi", methods=['GET', 'POST'])
-def pipi():
-    csv_to_mysql()
-    return 'OK'
-    # return render_template('kaka.html', form1=subject_form)  # , form2=reference_form)
-
-
-@app.route("/success/<title>", methods=['GET', 'POST'])
-def success(title):
-    sform = SearchSubject()
-    return '<h1>' + title + ' Great Success</h1>'
-    # return render_template('kaka.html', title='Great Success', sform=sform)
-
-
+# ++++++++++++  Home page ++++++++++++
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if 'GET' == request.method:
@@ -116,53 +122,6 @@ def home():
     # return redirect('/search_results', results)
 
 
-# ++++++++++++  search results and filtering page ++++++++++++
-@app.route('/search-results/<string:search_word>', methods=['GET', 'POST'])
-@app.route('/search-results', methods=['GET', 'POST'])
-def search_results(search_word=''):
-    flash('You doing great GRRRLLLL')
-
-    # search_word = search_word if search_word else request.args['search_word']
-    print(search_word)
-    if 'GET' == request.method:
-        # print(request.args)
-        # print(request.values)
-        # print(request)
-        # flash('You \'GET\' this')
-        # print('get')
-        # print(request.args['results'])
-        # results = request.args['results']
-        # return render_template('search-results.html', results=results)
-
-        subject_form = SearchSubject(request.form)
-        reference_form = SearchReference(request.form)
-        filter_form = FilterForm(request.form)
-
-        return render_template('search-results.html', title=f'Search Results for: {search_word}',
-                               description="Tiresias: The Ancient Mediterranean Religions Source Database",
-                               results=['res1', 'res2', 'res3', 'res4'], total=0,
-                               form1=subject_form, form2=reference_form, form3=filter_form,
-                               )  # , results=results)
-
-    flash('You\'re \'POST\' on!')
-    print('post')
-    # print(request.args['results'])
-    # results = request.args['results']
-    return render_template('search-results.html', title=f'Search Result for: {search_word}',
-                           description="Tiresias: The Ancient Mediterranean Religions Source Database",
-                           results=['pupu'], total=0)
-
-    # todo
-    #  in the search-results do
-    #    consider running the functions in "search-result" and NOT "home"
-    #    make some sort "waiting" bar/circle/notification (search "flashing/messages" in the flask doc)
-    #  change projection to include entire entry instead of index alone
-    #  add fuzzy (returns things *like* but not necessarily the same) / regex search on the query
-    #  clean data before:
-    #    split creation of tables function in db-migration into multiple function
-    #    appropriate normalization
-
-
 # ++++++++++++  list of books page ++++++++++++
 @app.route('/book-indices')
 def book_indices():
@@ -191,9 +150,6 @@ def subject_list():
     # there is not enough memory to do the next, but maybe consider the idea
     # subjects = TextSubject.query.order_by(
     #   TextSubject.subject.asc()).paginate(page, app.config['SUBJECTS_POSTS_PER_PAGE'], False)
-    print('=' * 20)
-    # for s in subjects.items:
-
     next_url = url_for('subject_list', page=subjects.next_num) if subjects.has_next else None
     prev_url = url_for('subject_list', page=subjects.prev_num) if subjects.has_prev else None
 
@@ -204,26 +160,57 @@ def subject_list():
                            next_url=next_url,
                            prev_url=prev_url
                            )
-    # return render_template(url_for('subject_list')+'.html')
 
 
-# ++++++++++++  dbg page ++++++++++++
+# ++++++++++++  dbg pages ++++++++++++
 @app.route('/check', methods=['GET', 'POST'])
 def check_check():
-    if request.method == 'GET':
-        return render_template('check.html', bookrefs=BookRef.query.all())
-    print(request.form.get("contents"))
-    print(request.form.get("contents2"))
-    if "contents2" in request.form:
-        field_contents = request.form["contents2"] + " (from 2nd field)"
-    else:
-        field_contents = request.form["contents"] + " (from 1st field)"
-    bookref = BookRef(titleref=field_contents, book_biblio_info=str(random.randint(3, 9)))
-    # db.session.add(bookref)
-    # db.session.commit()
-    return redirect('/check', bookref)
+    subject_form = SearchSubject(request.form)
+    # reference_form = SearchReference(request.form)
+    # if subject_form.submit_subject.data and subject_form.validate_on_submit():
+    #     print('Great Success')
+    #     return redirect(url_for('success', title='subject_form'))
+    # if reference_form.submit_reference.data and reference_form.validate_on_submit():
+    #     print('Great Success')
+    #     return redirect(url_for('success', title='reference_form'))
+    #     # return render_template('kaka.html', title='Great Success', subject_form=subject_form)
+    # print('what happend')
+    # print("subject_form.errors")
+    # print(subject_form.errors)
+    # flash(subject_form.errors)
+    # page = request.args.get('page', 1, type=int)
+    # table = TextSubject.query.paginate(page, app.config['POSTS_PER_PAGE'], False).items
+    # return render_template('kaka.html',mydata=table, form1=subject_form)  # , form2=reference_form)
+    return render_template('kaka.html', form1=subject_form)  # , form2=reference_form)
 
 
+@app.route("/kaka", methods=['GET', 'POST'])
+def kaka():
+    # headers = {"Content-Type": "app/kaka"}
+    # return make_response(
+    #     'Test worked!',
+    #     200,
+    #     headers
+    # )
+
+    return render_template('kaka.html')
+
+
+@app.route("/pipi", methods=['GET', 'POST'])
+def pipi():
+    csv_to_mysql()
+    return 'OK'
+    # return render_template('kaka.html', form1=subject_form)  # , form2=reference_form)
+
+
+@app.route("/success/<title>", methods=['GET', 'POST'])
+def success(title):
+    sform = SearchSubject()
+    return '<h1>' + title + ' Great Success</h1>'
+    # return render_template('kaka.html', title='Great Success', sform=sform)
+
+
+# ++++++++++++  Error Handling ++++++++++++
 @app.errorhandler(404)
 def not_found(error):
     resp = make_response(render_template('page_not_found.html',
