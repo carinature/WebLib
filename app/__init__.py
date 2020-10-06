585f1dc0abbf6c114a493aba16343d7e295d324e
karinature <karina@campus.technion.ac.il>
2020-10-06 15:46:14 +0000

    cbm

--- app/__init__.py
+++ app/__init__.py
@@ -6,13 +6,12 @@ db = SQLAlchemy()
 
 
 def create_app():
-    # print(' --- App Init --- ')
     app = Flask(__name__, instance_relative_config=False)
     # instance_relative_config=True tells the app that configuration files are relative to the instance folder.
     # The instance folder is located outside the flaskr package and can hold local data that shouldn’t be committed
     # to version control, such as configuration secrets and the database file.
-    app.config.from_object('config.DevConfig')  # development configuration
-    # app.config.from_object('config.ProdConfig')  # production configuration
+    # app.config.from_object('config.DevConfig')  # development configuration
+    app.config.from_object('config.ProdConfig')  # production configuration
 
     db.init_app(app)
 
@@ -20,7 +19,6 @@ def create_app():
     # csrf.init_app(app)
 
     with app.app_context():
-        # print(' - App CTX - ')
         # todo - make sure this line wasn't deleted:
         #  from . import routes  # Import routes
         from . import routes  # Import routes
