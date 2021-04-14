9aa79639a8df505d996ad46538fff77e4c29ee07
karinature <karina@campus.technion.ac.il>
2021-04-14 20:36:36 +0300

    cam

--- db_migration.py
+++ db_migration.py
@@ -27,6 +27,7 @@ class DBMigration:  # singleton class
                               defaults={
                                   'logfilename'    : f'logs/db_migration_{tt}.log',
                                   'fulllogfilename': f'logs/db_migration_full_{tt}.log',
+                                  'querylogfilename': f"logs/q_{tt}_empty.log",
                                   }
                               )
 
